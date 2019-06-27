99f67d884a6b58cef42e460375d6806613af4e60
Chase Wang <qiongbin.wang@yo-i.net>
2019-06-27 17:25:39 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='common_model',
+    version='0.0.1',
+    author='theggs',
+    license='The MIT License',
+    packages=find_packages(),
+)
